0958e06c1e95906a4465f2c68675edbd14419272
markus leonard <markus.leonard24@gmail.com>
2024-04-02 00:17:57 +0700

    fix flaky and refactor study_program.py

--- kurikulum/utils/mapper/study_program.py
+++ kurikulum/utils/mapper/study_program.py
@@ -7,11 +7,17 @@ from kurikulum.utils.mapper.IRI import IRI
 # return list of triple
 def mapping(wb: Workbook):
     ws = wb[Worksheet.STUDYPROGRAM.value]
+    min_row = 3
+    max_row = wb['Sheet8']['B1'].value + 2
 
     study_program = []
     iri_study_program = IRI(Prefix.OBE, 'StudyProgram')
 
-    for row in ws.iter_rows(min_row=3, values_only=True):
-        study_program.append([IRI(Prefix.OBE, row[0]), IRI(Prefix.RDF, 'type'), iri_study_program])
-        study_program.append([IRI(Prefix.OBE, row[0]), IRI(Prefix.OBE, 'spName'), IRI(Prefix.STRING, row[1])])
+    for row in ws.iter_rows(min_row=min_row, max_row=max_row, values_only=True):
+        iri_sp = IRI(Prefix.OBE, row[0])
+        iri_name = IRI(Prefix.STRING, row[1])
+
+        study_program.append([iri_sp, IRI(Prefix.RDF, 'type'), iri_study_program])
+        study_program.append([iri_sp, IRI(Prefix.OBE, 'spName'), iri_name])
+        
     return study_program
