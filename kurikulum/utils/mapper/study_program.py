7ab586c5ac5055ac5296039f4398504efb148c73
markus leonard <markus.leonard24@gmail.com>
2024-04-01 23:26:14 +0700

    implement study program mapper

--- kurikulum/utils/mapper/study_program.py
+++ kurikulum/utils/mapper/study_program.py
@@ -1,5 +1,7 @@
 from openpyxl import Workbook, load_workbook
+from kurikulum.enum.prefix import Prefix
 from kurikulum.enum.worksheet import Worksheet
+from kurikulum.utils.mapper.Node import IRI
 
 # study program
 # return list of triple
@@ -7,9 +9,9 @@ def mapping(wb: Workbook):
     ws = wb[Worksheet.STUDYPROGRAM.value]
 
     study_program = []
+    iri_study_program = IRI(Prefix.OBE, 'StudyProgram')
 
     for row in ws.iter_rows(min_row=3, values_only=True):
-        study_program.append(row)
-        print(row)
+        study_program.append([IRI(Prefix.OBE, row[0]), IRI(Prefix.RDF, 'type'), iri_study_program])
+        study_program.append([IRI(Prefix.OBE, row[0]), IRI(Prefix.OBE, 'spName'), IRI(Prefix.STRING, row[1])])
     return study_program
-
