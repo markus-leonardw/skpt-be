660261d934fe9742188797941fa5f73b71ed9d2a
markus leonard <markus.leonard24@gmail.com>
2024-04-03 02:18:13 +0700

    implement PEO mapper

--- kurikulum/utils/mapper/peo.py
+++ kurikulum/utils/mapper/peo.py
@@ -1,13 +1,25 @@
 from openpyxl import Workbook, load_workbook
 
+import kurikulum.const.predicate as predicate
+from kurikulum.enum.prefix import Prefix
 from kurikulum.enum.worksheet import Worksheet
+from kurikulum.utils.mapper.IRI import IRI
     
 def mapping(wb: Workbook):
     ws = wb[Worksheet.PEO.value]
+    min_row = 3
+    max_row = wb['Sheet8']['B3'].value + 2
 
-    peo_data = []
+    peo = []
+    iri_program_educational_objective = IRI(Prefix.OBE, 'ProgramEducationalObjective')
 
-    # Implement your mapping logic here
-    # Iterate through rows, extract data, and append to peo_data list
+    for row in ws.iter_rows(min_row=min_row, max_row=max_row, values_only=True):
+        iri_peo = IRI(Prefix.OBE, row[0])
+        iri_curr = IRI(Prefix.OBE, row[1])
+        iri_desc = IRI(Prefix.STRING, row[2])
 
-    return peo_data
+        peo.append([iri_peo, predicate.TYPE, iri_program_educational_objective])
+        peo.append([iri_peo, predicate.BELONGS_TO_CURR, iri_curr])
+        peo.append([iri_peo, predicate.DESCRIPTION, iri_desc])
+
+    return peo
