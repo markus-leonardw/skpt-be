178088642498601ff1f17bdaf05c8aa9d80141c3
markus leonard <markus.leonard24@gmail.com>
2024-04-24 04:34:22 +0700

    implement download upload template

--- kurikulum/views.py
+++ kurikulum/views.py
@@ -1,11 +1,17 @@
+from datetime import datetime
+from django.conf import settings
 from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.middleware import csrf
+from django.http import FileResponse
+import os
 from kurikulum.utils.mapper.mapper_service import Mapper
 from kurikulum.utils.request.delete_import_request import DeleteRequest
 from kurikulum.utils.request.execute_template_request import ExecuteTemplateRequest
 from kurikulum.utils.request.insert_import_request import InsertRequest
 from kurikulum.utils.request.sparql_query_request import ReadRequest
+from .forms import UploadFileForm
 
 def read(request):
     query = """
@@ -47,16 +53,14 @@ def delete_subject(request):
 
 @csrf_exempt
 def insert(request):
-    data = """
-PREFIX OBE: <http://www.semanticweb.org/ami/ontologies/2024/0/OBE#>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-OBE:course_markus_2 rdf:type OBE:Course .
-"""
+    mapper = Mapper()
+    ret = mapper.get_curriculum_data()
+    insert_query = mapper.construct_insert_query(ret)
 
-    import_name = "test_dummy"
+    import_name = "test_dummy_1"
 
     insert_request = InsertRequest()
-    insert_request.set_payload(import_name, data)
+    insert_request.set_payload(import_name, insert_query)
 
     response_insert = insert_request.make_request()
 
@@ -69,5 +73,31 @@ OBE:course_markus_2 rdf:type OBE:Course .
 
 def test(request):
     mapper = Mapper()
-    ret = mapper.get_study_program_data()
-    return HttpResponse(ret)
+    ret = mapper.get_course_data()
+    print(mapper.construct_insert_query(ret))
+    return HttpResponse(mapper.construct_insert_query(ret))
+
+def download_template(request):
+    template_path = './static/bulk_insert_template.xlsx'
+    return FileResponse(open(template_path, 'rb'), as_attachment=True)
+
+def download_template_display(request):
+    return render(request, 'download.html')
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            uploaded_file = form.cleaned_data['file']
+            # Define the path to save the uploaded file
+            save_path = os.path.join(settings.STATIC_ROOT, f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}_{uploaded_file.name}")
+            # Save the uploaded file
+            with open(save_path, 'wb+') as destination:
+                for chunk in uploaded_file.chunks():
+                    destination.write(chunk)
+            # Optionally, do something with the uploaded file
+            # Redirect or render a response
+            return render(request, 'upload_success.html')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
